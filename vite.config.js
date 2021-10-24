75b37097b23b06a9957cffb8e50e8b34cc405736
bholtbholt <bholtbholt@icloud.com>
2021-10-24 00:30:25 -0700

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/pet-names/',
   build: {
     outDir: './docs',
-    assetsDir: '',
+    assetsDir: './',
   },
 });
